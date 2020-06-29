c22fdec6dbca5bac9044576aed8f21d94a3e2964
Wayne Gong (minbgong) <minbgong@cisco.com>
2020-06-29 17:52:03 +0800

    Leetcode 335, Verify self-cross solution

---
+++ leetcode-solutions/leetcode-335.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+using namespace std;
+
+#if 0
+
+bool isSelfCrossing(vector<int>& x) {
+	if (x.size() >= 5 && x[3] == x[1] && x[0] + x[4] >= x[2]) {
+		return true;
+	}
+
+	for (int i = 3; i < x.size(); i++) {
+		if (x[i] >= x[i - 2] && x[i - 1] <= x[i - 3]) {
+			return true;
+		}
+		else if (i >= 5 && (x[i - 4] <= x[i - 2] && x[i] + x[i - 4] >= x[i - 2])
+			&& (x[i - 1] <= x[i - 3] && x[i - 1] + x[i - 5] >= x[i - 3])) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+#else
+
+bool isSelfCrossing(vector<int>& x) {
+	size_t i = 2;
+	int a = 0;
+
+	if (x.size() < 4)
+		return false;
+
+	while (x[i] > x[i - 2])
+		if (++i == x.size() - 1)
+			return false;
+
+	if (++i > 4)
+		if (x[i - 1] >= (x[i - 3] - x[i - 5]))
+			a = x[i - 4];
+
+	if (i > 3)
+		if (x[i - 1] == x[i - 3] && (x[i - 4] + x[i]) >= x[i - 2])
+			return true;
+
+	if (x[i] >= (x[i - 2] - a))
+		return true;
+
+	while (++i < x.size())
+		if (x[i] >= x[i - 2])
+			return true;
+
+	return false;
+}
+
+#endif
