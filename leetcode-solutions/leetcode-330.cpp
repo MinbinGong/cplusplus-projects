258bb58f7bc63c02b1b2a4e4663c9531106e1c45
Wayne Gong (minbgong) <minbgong@cisco.com>
2020-06-29 11:29:09 +0800

    Leetcode 330, Patching array solution

---
+++ leetcode-solutions/leetcode-330.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+int minPatches(vector<int> &nums, int n)
+{
+    int szNums = nums.size();
+    unsigned int farNum = 0;
+    int idx = 0, padding = 0;
+
+    while (farNum < n)
+    {
+        if (idx < szNums && nums[idx] <= farNum + 1)
+        {
+            farNum += nums[idx++];
+        }
+        else
+        {
+            padding++;
+            farNum = farNum * 2 + 1;
+        }
+    }
+    return padding;
+}
