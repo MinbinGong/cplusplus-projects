65bea1dd0046847abaa471e2654b17c1d4939bb0
Wayne Gong (minbgong) <minbgong@cisco.com>
2020-07-01 09:00:50 +0800

    Leetcode 343, Integer break solution

---
+++ leetcode-solutions/leetcode-343.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+using namespace std;
+
+int integerBreak(int n)
+{
+    vector<int> dp(n + 1, 1);
+    for (int j = 2; j <= n; j++)
+    {
+        for (int i = 1; i < j; i++)
+        {
+            dp[j] = max(dp[j], i * (j - i));
+            dp[j] = max(dp[j], i * dp[j - i]);
+        }
+    }
+    return dp[n];
+}
