5720a4a7f7db41055e0477960fb6a3ae816c3a0d
Wayne Gong <gongwayne@hotmail.com>
2020-08-07 22:49:13 +0800

    solution for leetcode-373

---
+++ leetcode-solutions/leetcode-373.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <priority_queue>
+#include <utility>
+using namespace std;
+
+vector<vector<int>> kSmallestPairs(vector<int> &nums1, vector<int> &nums2, int k)
+{
+    priority_queue<pair<int, pair<int, int>>> mxheap;
+    for (auto x : nums1)
+    {
+        for (auto y : nums2)
+        {
+            if (mxheap.size() >= k)
+            {
+                if (mxheap.top().first > (x + y))
+                {
+                    mxheap.pop();
+                    mxheap.push({x + y, {x, y}});
+                }
+            }
+            else
+                mxheap.push({x + y, {x, y}});
+        }
+    }
+    vector<vector<int>> res;
+    while (mxheap.size() != NULL)
+    {
+        auto x = mxheap.top();
+        mxheap.pop();
+        res.push_back({x.second.first, x.second.second});
+    }
+    //reverse(res.begin(),res.end());
+    return res;
+}
