ea4802e191ce271d204d154462c2bd186de0399c
Wayne Gong <gongwayne@hotmail.com>
2020-08-07 22:48:58 +0800

    solution for leetcode-372

---
+++ leetcode-solutions/leetcode-372.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+int powmod(int a, int k) //a^k mod 1337 where 0 <= k <= 10
+{
+    a %= base;
+    int result = 1;
+    for (int i = 0; i < k; ++i)
+        result = (result * a) % base;
+    return result;
+}
+
+int superPow(int a, vector<int> &b)
+{
+    if (b.empty())
+        return 1;
+    int last_digit = b.back();
+    b.pop_back();
+    return powmod(superPow(a, b), 10) * powmod(a, last_digit) % base;
+}
