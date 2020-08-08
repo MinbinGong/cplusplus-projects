06759d779cb51d61bb9fd0f90a3618e9a9bf64da
Wayne Gong <gongwayne@hotmail.com>
2020-08-08 14:27:54 +0800

    Format the source file in leetcode-372

--- leetcode-solutions/leetcode-372.cpp
+++ leetcode-solutions/leetcode-372.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int powmod(int a, int k) //a^k mod 1337 where 0 <= k <= 10
 {
-    a %= base;
+    a %= 1337;
     int result = 1;
     for (int i = 0; i < k; ++i)
-        result = (result * a) % base;
+        result = (result * a) % 1337;
     return result;
 }
 
@@ -16,5 +16,5 @@ int superPow(int a, vector<int> &b)
         return 1;
     int last_digit = b.back();
     b.pop_back();
-    return powmod(superPow(a, b), 10) * powmod(a, last_digit) % base;
+    return powmod(superPow(a, b), 10) * powmod(a, last_digit) % 1337;
 }
