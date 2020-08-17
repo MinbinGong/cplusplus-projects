5bceeb724b474704e46afcd6b609b1e4c51baa8a
Wayne Gong <gongwayne@hotmail.com>
2020-08-17 20:42:25 +0800

    Solution for leetcode 386, Lexicographical numbers

---
+++ leetcode-solutions/leetcode-386.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+vector<int> lexicalOrder(int n)
+{
+    if (n == 0)
+        return {};
+    vector<int> result;
+    int current = 1; //Initial element
+    for (int i = 0; i < n; i++)
+    {
+        result.push_back(current); //Push current to the result.
+        current *= 10;             // Add zero at the end of current.
+        while (current > n)
+        {                  //If current exceeds n.
+            current /= 10; //Fall back to last element.
+            current++;     //Get Next in order.
+            while (current % 10 == 0)
+                current /= 10; //Remove extra trailing zeros.
+        }
+    }
+    return result;
+}
