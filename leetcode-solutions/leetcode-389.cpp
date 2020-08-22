e1746c97c817e3b407cc79c0c384bfded0e2e57a
Wayne Gong <gongwayne@hotmail.com>
2020-08-22 21:30:13 +0800

    Solution for leetcode 389, find the difference in strings

---
+++ leetcode-solutions/leetcode-389.cpp
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+
+char findTheDifference(string s, string t)
+{
+    int temp = 0;
+    for (char c : t)
+        temp += c - 'a';
+    for (char c : s)
+        temp -= c - 'a';
+    return temp + 'a';
+}
