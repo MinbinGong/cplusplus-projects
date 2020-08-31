ec5d735c963cc92f6f9c5e472d8c6691d20fd797
Wayne Gong <minbgong@cisco.com>
2020-08-31 15:52:20 +0800

    Solution for leetcode 397, integer replacement

---
+++ leetcode-solutions/leetcode-397.cpp
@@ -0,0 +1,21 @@
+#include <climits>
+int integerReplacement(int n)
+{
+    if (n == INT_MAX)
+        return 32;
+    int i = 1, c = 0;
+    while (n != 1)
+    {
+        if (n % 2 == 0)
+            n /= 2; //if n is even simply n=n/2
+        else
+        {
+            if (((n - 1) / 2) % 2 == 0 || (n - 1) / 2 == 1)
+                n -= 1;
+            else
+                n++;
+        }
+        c++;
+    }
+    return c;
+}
