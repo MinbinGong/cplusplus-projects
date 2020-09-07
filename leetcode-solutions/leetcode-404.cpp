30c7080c0c8cdbe943928e74b36aa72f1796c8dc
Wayne Gong <gongwayne@hotmail.com>
2020-09-07 20:56:29 +0800

    Solution for leetcode 404, Sum of left leaves

---
+++ leetcode-solutions/leetcode-404.cpp
@@ -0,0 +1,51 @@
+#include <set>
+#include <queue>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int sumOfLeftLeaves(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int sum = 0;
+    set<TreeNode *> s;
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+
+        TreeNode *temp = q.front();
+        if (temp->left == NULL && temp->right == NULL)
+        {
+
+            if (s.find(temp) != s.end())
+            {
+                sum = sum + temp->val;
+            }
+        }
+
+        if (temp->left != NULL)
+        {
+            q.push(temp->left);
+            s.insert(temp->left);
+        }
+        if (temp->right != NULL)
+        {
+            q.push(temp->right);
+        }
+        q.pop();
+    }
+    s.clear();
+    return sum;
+}
