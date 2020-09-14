54fee981f51a1450283e7f5517af777c9519618b
Wayne Gong <minbgong@cisco.com>
2020-09-14 14:47:54 +0800

    std::minmax example

---
+++ stl_practices/source_codes/util/minmax1.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+
+bool int_ptr_less(int *a, int *b)
+{
+    return *a < *b;
+}
+
+int main()
+{
+    int x = 17, y = 42, z = 33;
+
+    int *px = &x, *py = &y, *pz = &z;
+
+    int *pmax = std::max(px, py, int_ptr_less);
+
+    std::pair<int *, int *> extremes = std::minmax({px, py, pz}, int_ptr_less);
+
+    return 0;
+}
