bc539cf812de1dae7e6de3b484a9116f70da0e54
Wayne Gong <minbgong@cisco.com>
2020-09-15 14:55:20 +0800

    example of remove algorithm

---
+++ stl_practices/source_codes/util/remove1.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iterator>
+#include <list>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    list<int> coll;
+    for (size_t i = 1; i <= 6; i++)
+    {
+        coll.push_back(i);
+        coll.push_front(i);
+    }
+
+    cout << "Pre: ";
+    copy(coll.cbegin(), coll.cend(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    auto end = remove(coll.begin(), coll.end(), 3);
+
+    cout << "Post: ";
+    copy(coll.begin(), end, ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    cout << "number of removed elements: " << distance(end, coll.end()) << endl;
+
+    coll.erase(end, coll.end());
+
+    copy(coll.begin(), end, ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    return 0;
+}
