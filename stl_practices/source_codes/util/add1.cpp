515d9552df37053231e433fb3fad5f1614ddde7f
Wayne Gong <minbgong@cisco.com>
2020-09-15 16:06:02 +0800

    example of function object

---
+++ stl_practices/source_codes/util/add1.cpp
@@ -0,0 +1,33 @@
+#include <list>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+class AddValue
+{
+private:
+    int theValue;
+
+public:
+    AddValue(int v) : theValue(v)
+    {
+    }
+
+    void operator()(int &elem) const
+    {
+        elem += theValue;
+    }
+};
+
+int main()
+{
+    list<int> coll;
+    for (size_t i = 1; i <= 9; i++)
+    {
+        coll.push_back(i);
+    }
+
+    for_each(coll.begin(), coll.end(), AddValue(10));
+
+    return 0;
+}
