5ae9c7d07a0937d26e0a725324a3dcb3859ccef4
Wayne Gong <2004wayne@163.com>
2020-09-20 14:24:53 +0800

    Solution for leetcode 414, third maximum number

---
+++ leetcode-solutions/leetcode-414.cpp
@@ -0,0 +1,14 @@
+#include <set>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int thirdMax(vector<int> &nums)
+{
+    set<int> num(nums.begin(), nums.end());
+    if (num.size() < 3)
+        return *(num.rbegin());
+    auto it = num.rbegin();
+    advance(it, 2);
+    return *it;
+}
