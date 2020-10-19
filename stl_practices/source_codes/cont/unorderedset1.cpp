ee4d94d61853dbf8f75dc217bd28838dabfb6172
Wayne Gong <2004wayne@163.com>
2020-10-19 21:42:35 +0800

    Example of unordered_set usage

---
+++ stl_practices/source_codes/cont/unorderedset1.cpp
@@ -0,0 +1,54 @@
+#include <unordered_set>
+#include <numeric>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unordered_set<int> coll{1, 2, 3, 5, 7, 11, 13, 17, 19, 77};
+    for (const auto &e : coll)
+    {
+        cout << e << " ";
+    }
+    cout << endl;
+
+    coll.insert({-7, 17, 33, -11, 17, 19, 1, 13});
+    for (const auto &e : coll)
+    {
+        cout << e << " ";
+    }
+    cout << endl;
+
+    coll.erase(33);
+
+    coll.insert(accumulate(coll.begin(), coll.end(), 0));
+    for (const auto &e : coll)
+    {
+        cout << e << " ";
+    }
+    cout << endl;
+
+    if (coll.find(19) != coll.end())
+    {
+        cout << "19 is available" << endl;
+    }
+
+    for (auto pos = coll.begin(); pos != coll.end();)
+    {
+        if (*pos < 0)
+        {
+            pos = coll.erase(pos);
+        }
+        else
+        {
+            ++pos;
+        }
+    }
+    for (const auto &e : coll)
+    {
+        cout << e << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
