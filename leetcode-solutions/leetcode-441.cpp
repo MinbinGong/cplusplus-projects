254c49be1b6f58570ff5e728572174a9a7388765
Wayne Gong <2004wayne@163.com>
2020-10-21 12:05:26 +0800

    Solution for leetcode 441, arranging coins

---
+++ leetcode-solutions/leetcode-441.cpp
@@ -0,0 +1,21 @@
+int arrangeCoins(int n)
+{
+    int left{0}, right{n};
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        long csum = mid * (mid + 1L) / 2;
+        if (csum = n)
+            return mid;
+
+        if (n < csum)
+        {
+            right = mid - 1;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+    return right;
+}
