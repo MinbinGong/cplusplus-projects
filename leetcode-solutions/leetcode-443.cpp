f7939e764f312154259f55ae4fcd128f851d2e09
Wayne Gong <2004wayne@163.com>
2020-10-21 12:46:28 +0800

    Solution for leetcode 443, string compression

---
+++ leetcode-solutions/leetcode-443.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+using namespace std;
+
+int compress(vector<char> &chars)
+{
+    int i = 0;
+    int j = 0;
+    int len = chars.size();
+
+    while (i < len)
+    {
+        int cnt = 1;
+        chars[j++] == chars[i++];
+        while (i < len && chars[j - 1] == chars[i])
+        {
+            i++;
+            cnt++;
+        }
+
+        if (cnt > 1)
+        {
+            for (const auto &c : to_string(cnt))
+            {
+                chars[j++] = c;
+            }
+        }
+
+        return j;
+    }
+}
