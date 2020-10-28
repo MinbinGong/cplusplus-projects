4b6782d6c59a7b75fa2992e57e16cc684310a362
Wayne Gong <2004wayne@163.com>
2020-10-28 22:28:54 +0800

    Solution for leetcode 3, longest substring without repeating characters

---
+++ leetcode-solutions/leetcode-3.cpp
@@ -0,0 +1,40 @@
+#include <string>
+using namespace std;
+
+int lengthOfLongestSubstring(string s)
+{
+    int array[95] = {0};
+    int maxLen = 0;
+    int temp = 0;
+    int windowStart = 0;
+    for (size_t i = 0; i < s.length(); ++i)
+    {
+        if (array[s[i] - ' '] == 0)
+        {
+            array[s[i] - ' '] = i + 1;
+            ++temp;
+        }
+        else
+        {
+            if (temp > maxLen)
+            {
+                maxLen = temp;
+            }
+
+            if (array[s[i] - ' '] > windowStart)
+            {
+                windowStart = array[s[i] - ' '];
+            }
+
+            temp = i + 1 - windowStart;
+            array[s[i] - ' '] = i + 1;
+        }
+    }
+
+    if (temp > maxLen)
+    {
+        maxLen = temp;
+    }
+
+    return maxLen;
+}
