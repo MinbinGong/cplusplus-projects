f9643d609579aab42c9ebc40f95ea4d6a4be8367
Wayne Gong <2004wayne@163.com>
2020-10-28 21:45:05 +0800

    Solution for leetcode 137, single number II

---
+++ leetcode-solutions/leetcode-137.cpp
@@ -0,0 +1,28 @@
+/*
+ * Given a non-empty array of integers, every element appears three times except for one,
+ * which appears exactly once. 
+ * Find that single one.
+ */
+
+#include <vector>
+using namespace std;
+
+int singleNumber(vector<int> &A)
+{
+    if (A.size() == 0)
+    {
+        return 0;
+    }
+
+    int first = 0, second = 0;
+    int szA = A.size();
+    for (int i = 0; i < szA; i++)
+    {
+        second |= (first & A[i]);
+        first ^= A[i];
+        int check = ~(first & second);
+        first &= check;
+        second &= check;
+    }
+    return first;
+}
