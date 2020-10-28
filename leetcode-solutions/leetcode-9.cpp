aa018f12181da540e4379800b9d06385d926767d
Wayne Gong <2004wayne@163.com>
2020-10-28 22:51:19 +0800

    Solution for leetcode 9, Palindrome number

---
+++ leetcode-solutions/leetcode-9.cpp
@@ -0,0 +1,34 @@
+/*
+ * Palindrome number
+ */
+#include <string>
+using namespace std;
+
+bool isPalindrome(int x)
+{
+    if (x < 0)
+        return false;
+
+    string exp = to_string(x);
+    int len = exp.length();
+
+    if (len == 1)
+        return true;
+
+    int rem = len % 2, half = len / 2, i = half - 1, j = 0;
+
+    if (rem == 0) //even length
+        j = half;
+    else //odd length
+        j = half + 1;
+
+    while (i >= 0)
+    {
+        if (exp[i--] != exp[j++])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
