12b1b724e65f8d83ea684611252a6a9848f5496c
Wayne Gong <2004wayne@163.com>
2020-11-02 16:50:25 +0800

    Solution for leetcode 473, matchsticks to square

---
+++ leetcode-solutions/leetcode-473.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <algorithm>
+#include <bitset>
+#include <numeric>
+using namespace std;
+
+bool dfs(int index, int sum, vector<int> &nums, bitset<15> &track)
+{
+    if (sum == 0)
+    {
+        return true;
+    }
+
+    for (size_t i = index; i < nums.size(); ++i)
+    {
+        if (!track[i] && (sum - nums[i] >= 0))
+        {
+            track[i] = true;
+
+            if (dfs(i + 1, sum - nums[i], nums, track))
+            {
+                return true;
+            }
+            track[i] = false;
+        }
+    }
+    return false;
+}
+
+bool makesquare(vector<int> &nums)
+{
+    if (nums.size() == 0)
+    {
+        return false;
+    }
+
+    sort(nums.begin(), nums.end(), greater<int>());
+
+    bitset<15> track;
+    int side = accumulate(nums.begin(), nums.end(), 0) / 4;
+    for (size_t i = 0; i < 3; ++i)
+    {
+        if (!dfs(0, side, nums, track))
+        {
+            return false;
+        }
+    }
+    return true;
+}
