3e0946e8d8b0f4eab717680248bd5ab103eeeeb1
Wayne Gong <gongwayne@hotmail.com>
2023-01-11 11:25:45 +0800

    Add more leetcode solutions

--- leetcode-solutions/leetcode-30.cpp
+++ leetcode-solutions/leetcode-30.cpp
@@ -1,3 +1,13 @@
+// Leetcode 30,  Substring with Concatenation of All Words
+
+// Approach : We are using Hashing & Sliding Window Technique to solve the above problem.
+// 1.Declare a map wordCount which stores all words count
+// 2.Select a window of size wordsWindow(total number of characters produced if all the words are concatenated)
+// 3.Check if that window is possible to build using our wordCount map.
+// 4.If True, add the starting index to our result vector
+// 5.Increasing the window by 1 & check until we are able to select wordsWindow size window.
+//
+
 #include <unordered_map>
 #include <string>
 #include <vector>
