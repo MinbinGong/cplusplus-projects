8d700363236564ac7f0f24f3f8504b61c5028369
Wayne Gong <gongwayne@hotmail.com>
2023-01-29 04:34:22 +0000

    Add more exercise

---
+++ algorithm-essentials/UniqueBST/UniqueBST.cpp
@@ -0,0 +1,26 @@
+// UniqueBST.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+
+int numTrees(int n) {
+    std::vector<int> f(n + 1, 0);
+
+    f[0] = 1;
+    f[1] = 1;
+    for (size_t i = 2; i <= n; i++)
+    {
+        for (size_t k = 1; k <= i; k++)
+        {
+            f[i] += f[k - 1] * f[i - k];
+        }
+    }
+
+    return f[n];
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
