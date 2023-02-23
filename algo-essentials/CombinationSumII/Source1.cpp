22e9424f94c4e97542e65dde43ab78acd882405e
Wayne Gong <gongwayne@hotmail.com>
2023-02-23 20:45:07 +0800

    Add more exercises

---
+++ algo-essentials/CombinationSumII/Source1.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <algorithm>
+
+void dfs(const std::vector<int>& nums, std::vector<int>& path,
+    std::vector<std::vector<int>>& result, int gap, int start)
+{
+    if (gap == 0)
+    {
+        result.push_back(path);
+        return;
+    }
+
+    int previous = -1;
+    for (size_t i = start; i < nums.size(); i++)
+    {
+        if (previous == nums[i])
+        {
+            continue;
+        }
+
+        if (gap < nums[i])
+        {
+            break;
+        }
+
+        previous = nums[i];
+
+        path.push_back(nums[i]);
+        dfs(nums, path, result, gap - nums[i], i + 1);
+        path.pop_back();
+    }
+}
+
+std::vector<std::vector<int>> combinationSum2(std::vector<int>& nums, int target)
+{
+    std::sort(nums.begin(), nums.end());
+
+    std::vector<std::vector<int>> result;
+    std::vector<int> path;
+    dfs(nums, path, result, target, 0);
+    return result;
+}
---
+++ algo-essentials/GenerateParentheses/Source1.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+void generateHelper(int n, std::string& path, std::vector<std::string>& result, int l, int r)
+{
+
+}
+
+std::vector<std::string> generateParenthesis(int n)
+{
+    std::vector<std::string> result;
+    if (n <= 0)
+    {
+        return result;
+    }
+
+    std::string path;
+    generateHelper(n, path, result, 0, 0);
+    return result;
+}
