b98af4dcc53df9eaf5b65942377a07e85812fd0a
Wayne Gong <gongwayne@hotmail.com>
2023-08-29 19:49:10 +0800

    Add more solutions for leetcode problems

--- leetcode-solutions/leetcode-36.cpp
+++ leetcode-solutions/leetcode-36.cpp
@@ -10,4 +10,49 @@
  * solvable. Only the filled cells need to be validated according to the
  * mentioned rules.  
  */
+#include <vector>
+#include <set>
 
+bool isValidSudoku(std::vector<std::vector<char>>& board) {
+    std::set<int> foundx, foundy;
+
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (foundx.find(board[i][j] - '0') != foundx.end()) {
+                return false;
+            } else if (board[i][j] != '.') {
+                foundx.insert(board[i][j] - '0');
+            }
+
+            if (foundy.find(board[j][i] - '0') != foundy.end()) {
+                return false;
+            } else if (board[j][i] != '.') {
+                foundy.insert(board[j][i] - '0');
+            }
+        }
+        foundx.clear();
+        foundy.clear();
+    }
+
+    foundx.clear();
+    
+    std::vector<std::vector<int>> boxes = {
+        {0, 0}, {0, 3}, {0, 6},
+        {3, 0}, {3, 3}, {3, 6},
+        {6, 0}, {6, 3}, {6, 6}
+    };
+
+    for (auto b: boxes) {
+        for (int i = b[0]; i < b[0] + 3; i++) {
+            for (int j = b[1]; j < b[1] + 3; j++) {
+                if (foundx.find(board[i][j] - '0') != foundx.end()) {
+                    return false;
+                } else if (board[i][j] != '.') {
+                    foundx.insert(board[i][j] - '0');
+                }
+            }
+        }
+        foundx.clear();
+    }
+    return true;
+}
---
+++ leetcode-solutions/leetcode-38.cpp
@@ -0,0 +1,38 @@
+/*
+ * The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+ *  countAndSay(1) = "1"
+ *  countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is
+ *  then converted into a different digit string.
+ * To determine how you "say" a digit string, split it into the minimal number of substrings
+ * such that each substring contains exactly one unique digit. Then for each substring, say the
+ * number of digits, then say the digit. Finally, concatenate every said digit.
+ */
+#include <string>
+
+std::string fun(std::string str1) {
+    str1 += " ";
+    int len = str1.size();
+    int count = 1;
+    std::string ans = "";
+
+    for (int i = 0; i < len - 1; i++) {
+        if (str1[i + 1] == str1[i]) {
+            count++;
+            continue;
+        }
+
+        ans += ('0' + count);
+        ans += str1[i];
+        count = 1;
+    }
+
+    return ans;
+}
+
+std::string countAndSay(int n) {
+    if (n == 1) return "1";
+
+    std::string x = func(countAndSay(n-1));
+    return x;
+}
+
