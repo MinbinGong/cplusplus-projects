5b1ceeb31e7b68a0ad26d8d2a420ed685382a72e
Wayne Gong <gongwayne@hotmail.com>
2023-08-31 21:30:44 +0800

    Add more solutions for leetcode problems

--- leetcode-solutions/leetcode-39.cpp
+++ leetcode-solutions/leetcode-39.cpp
@@ -34,4 +34,31 @@
  * All elements of candidates are distinct.
  * 1 <= target <= 40
  */
+#include <vector>
+
+void find_combination(int index, int target, std::vector<int>& candidates,
+        std::vector<std::vector>>& ans, std::vector<int>& ds) {
+    if (index == candidates.size()) {
+        if (target == 0) {
+            ans.push_back(ds);
+        }
+        return;
+    }
+
+    if (candidates[index] <= target) {
+        ds.push_back(candidates[index]);
+        find_combination(index, target - candidates[index], candidates, ans, ds);
+        ds.pop_back();
+    }
+
+    find_combination(index + 1, target, candidates, ans, ds);
+}
+
+std::vector<std::vector<int>> combination_sum(std::vector<int>& candidates,
+        int target) {
+    std::vector<std::vector<int>> ans;
+    std::vector<int> ds;
+    find_combination(0, target, candidates, ans, ds);
+    return ans;
+}
 
