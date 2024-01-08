00e7ba552ed78c2fc9be461cc7fe7a7350500062
Wayne Gong <gongwayne@hotmail.com>
2024-01-08 21:03:57 +0800

    Add more leetcode solutions

--- leetcode-solutions/leetcode-1143.cpp
+++ leetcode-solutions/leetcode-1143.cpp
@@ -1,3 +1,20 @@
+/*
+    Longest Common Subsequence
+
+    题目描述
+        给定两个字符串，求它们最长的公共子序列长度。
+
+    题解
+        对于子序列问题，第二种动态规划方法是，定义一个 dp 数组，其中 dp[i] 表示到位置 i 为止
+    的子序列的性质，并不必须以 i 结尾。这样 dp 数组的最后一位结果即为题目所求，不需要再对每
+    个位置进行统计。
+        在本题中，我们可以建立一个二维数组 dp，其中 dp[i][j] 表示到第一个字符串位置 i 为止、到
+    第二个字符串位置 j 为止、最长的公共子序列长度。这样一来我们就可以很方便地分情况讨论这
+    两个位置对应的字母相同与不同的情况了。
+ */
+#include <string>
+using namespace std;
+
 int longestCommonSubsequence(string text1, string text2) {
     int n = text1.size(), m = text2.size();
     int dp[m + 1][n + 1];
@@ -19,4 +36,8 @@ int longestCommonSubsequence(string text1, string text2) {
         }
     }
     return dp[m][n];
+}
+
+int longestCommonSubsequence1(string text1, string text2) {
+    
 }
---
+++ leetcode-solutions/leetcode-139.cpp
@@ -0,0 +1,26 @@
+/*
+  题目描述
+    给定一个字符串和一个字符串集合，求是否存在一种分割方式，使得原字符串分割后的子字符串都可以在集合内找到。
+
+  题解
+    类似于完全平方数分割问题，这道题的分割条件由集合内的字符串决定，因此在考虑每个分割位置时，需要遍历字符串集合，
+  以确定当前位置是否可以成功分割。注意对于位置 0，需要初始化值为真。
+ */
+#include <string>
+#include <vector>
+using namespace std;
+
+bool workBreak(string &s, vector<string> &wordDict) {
+    int n = s.length();
+    vector<bool> dp(n + 1, false);
+    dp[0] = true;
+    for (int i = 1; i <= n; ++i) {
+        for (const auto &word : wordDict) {
+            int len = word.length();
+            if (i >= len && s.substr(i - len, len) == word) {
+                dp[i] = dp[i] || dp[i - len];
+            }
+        }
+    }
+    return dp[n];
+}
