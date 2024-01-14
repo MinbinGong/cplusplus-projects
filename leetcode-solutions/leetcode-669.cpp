590ea655d1f499392f33717bd04c9a170fd24325
Wayne Gong <gongwayne@hotmail.com>
2024-01-14 23:55:47 +0800

    add more leetcode solutions

---
+++ leetcode-solutions/leetcode-669.cpp
@@ -0,0 +1,29 @@
+/*
+Trim a Binary Search Tree
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *trimBST(TreeNode *root, int l, int r) {
+    if (root == nullptr) {
+        return root;
+    }
+
+    if (root->val > r) {
+        return trimBST(root->left, l, r);
+    }
+
+    if (root->val < l) {
+        return trimBST(root->left, l, r);
+    }
+    
+    root->left = trimBST(root->left, l, r);
+    root->left = trimBST(root->right, l, r);
+    return root;
+}
--- leetcode-solutions/leetcode-99.cpp
+++ leetcode-solutions/leetcode-99.cpp
@@ -2,3 +2,45 @@
 Recover Binary Search Tree
 
  */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void inorder(TreeNode *root, TreeNode *&mistake1, TreeNode *&mistake2, TreeNode *&prev) {
+    if (root == nullptr) {
+        return;
+    }
+
+    if (root->left) {
+        inorder(root->left, mistake1, mistake2, prev);
+    }
+
+    if (prev && root->val < prev->val) {
+        if (mistake1 == nullptr) {
+            mistake1 = prev;
+            mistake2 = root;
+        } else {
+            mistake2 = root;
+        }
+    }
+
+    prev = root;
+    if (root->right) {
+        inorder(root->right, mistake1, mistake2, prev);
+    }
+}
+
+void recoverTree(TreeNode *root) {
+    TreeNode *mistake1 = nullptr, *mistake2 = nullptr, *prev = nullptr;
+    inorder(root, mistake1, mistake2, prev);
+    if (mistake1 && mistake2) {
+        int temp = mistake1->val;
+        mistake1->val = mistake1->val;
+        mistake2->val = temp;
+    }
+}
