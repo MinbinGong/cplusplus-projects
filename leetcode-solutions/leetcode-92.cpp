3885b79010a817383fe0bba22dd381c690ac16c0
Wayne Gong <gongwayne@hotmail.com>
2024-02-03 21:41:59 +0800

    Add more leetcode solutions

--- leetcode-solutions/leetcode-92.cpp
+++ leetcode-solutions/leetcode-92.cpp
@@ -13,4 +13,56 @@ struct ListNode {
     ListNode(int x, ListNode *n) : val(x), next(n) {}
 };
 
+ListNode *reverseBetween(ListNode *head, int left, int right) {
+    if (head == nullptr) {
+        return head;
+    }
 
+    ListNode *reversedHead = nullptr;
+    ListNode *reversedTail = nullptr;
+
+    ListNode *before = nullptr;
+    ListNode *temp = head;
+    ListNode *after = nullptr;
+
+    int index = 1;
+    while (temp) {
+        after = temp->next;
+
+        if (index < left) {
+            before = temp;
+        } else if (index >= left && index <= right) {
+            if (before && before->next) {
+                before->next = nullptr;
+            }
+
+            temp->next = nullptr;
+            
+            if (reversedHead == nullptr) {
+                reversedHead = temp;
+                reversedTail = temp;
+            } else {
+                temp->next = reversedHead;
+                reversedHead = temp;
+            }
+        }
+
+        temp = after;
+
+        if (index == right) {
+            break;
+        }
+
+        index++;
+    }
+
+    if (before) {
+        before->next = reversedHead;
+    } else {
+        head = reversedHead;
+    }
+
+    reversedTail->next = after;
+
+    return head;
+}
