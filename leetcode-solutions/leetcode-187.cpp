9816a997fa51df583d750282961d4ba6bfa0fe9e
Wayne Gong <gongwayne@hotmail.com>
2024-05-01 20:50:31 +0800

    Add more leetcode solution

--- leetcode-solutions/leetcode-187.cpp
+++ leetcode-solutions/leetcode-187.cpp
@@ -12,7 +12,7 @@
  * 10-letter-long sequences (substrings) that occur more than once in a DNA
  * molecule. You may return the answer in any order.
  *
- *  
+ *
  *
  * Example 1:
  *
@@ -22,15 +22,50 @@
  *
  * Input: s = "AAAAAAAAAAAAA"
  * Output: ["AAAAAAAAAA"]
- *  
+ *
  * Constraints:
  *
  * 1 <= s.length <= 105
  * s[i] is either 'A', 'C', 'G', or 'T'.
  */
 #include <string>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 
 vector<string> findRepeatedDnaSequences(string s) {
+  vector<string> res;
+  long long hash = 0;
+  int n = s.length();
+  vector<int> inp(n);
+  unordered_map<long long, int> m;
+  if (n < 10) {
+    return res;
+  }
 
+  for (int i = 0; i < n; i++) {
+    if (s[i] == 'A') {
+      inp[i] = 1;
+    } else if (s[i] == 'C') {
+      inp[i] = 2;
+    } else if (s[i] == 'G') {
+      inp[i] = 3;
+    } else {
+      inp[i] = 4;
+    }
+  }
+
+  for (int i = 0; i < 10; i++) {
+    hash = hash * 4 + inp[i];
+  }
+
+  m[hash]++;
+  for (int i = 1; i < n - 9; i++) {
+    hash = hash * 4 - inp[i - 1] * pow(4, 10) + inp[i + 9];
+    m[hash]++;
+    if (m[hash] == 2) {
+      res.push_back(s.substr(i, 10));
+    }
+  }
+  return res;
+}
